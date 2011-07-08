d44bea8ec8c444dfd3b58badbed156481f4ea823
David Woods <dwoodsnz@gmail.com>
2011-07-08 19:59:59 +1000

    changed tabs to spaces in p1 java

--- java/problems/Problem1.java
+++ java/problems/Problem1.java
@@ -5,17 +5,17 @@ package projecteuler.problems;
  * up to the limit.
  */
 public class Problem1 {
-	
-	public static void main(String[] args) {
-		int limit = 1000;
-		int total = 0;
-		
-		for (int i=1; i<limit; i++) {
-			if (i % 3 == 0 || i % 5 == 0) {
-				total += i;
-			}
-		}
-		
-		System.out.println("total=" + total);
-	}
+    
+    public static void main(String[] args) {
+        int limit = 1000;
+        int total = 0;
+        
+        for (int i=1; i<limit; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                total += i;
+            }
+        }
+        
+        System.out.println("total=" + total);
+    }
 }
