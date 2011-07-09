82aa94fd376f2fc32e069c7549d95402689e43a6
David Woods <dwoodsnz@gmail.com>
2011-07-09 18:27:52 +1000

    solved p1 in clojure

---
+++ clojure/Problem1.clj
@@ -0,0 +1,8 @@
+(ns Problem1)
+
+(defn multiple-of-3-or-5? [x]
+  (or (= 0 (mod x 3))
+      (= 0 (mod x 5))))
+
+(defn sum-multiples-of-3-or-5 [n]
+  (apply + (filter multiple-of-3-or-5? (range 1 n))))
