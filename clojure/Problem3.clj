c7a42c0a00d4c3dd96c2762856ad022c3de8c3a2
David Woods <dwoodsnz@gmail.com>
2011-07-10 20:07:15 +1000

    solved p3 in clojure

---
+++ clojure/Problem3.clj
@@ -0,0 +1,22 @@
+(ns Problem3)
+
+(defn prime?
+  "Checks if a given number is prime"
+  [x]
+  (loop [i 2]
+    (if (> i (Math/sqrt x))
+        true
+        (if (= 0 (mod x i))
+            false
+            (recur (inc i))))))
+
+(defn largest-prime-factor [x]
+  (loop [i 2]
+    (let [j (/ x i)]
+      (if (and (integer? j) (= 0 (mod x j)))
+          (if (prime? j)
+            j
+            (recur (inc i)))
+          (recur (inc i))))))
+      
+(println (largest-prime-factor 600851475143))
