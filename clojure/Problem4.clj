184131c46b3116592fd7035338fd59967be87e4c
David Woods <dwoodsnz@gmail.com>
2011-07-10 20:59:42 +1000

    solved p4 in clojure

---
+++ clojure/Problem4.clj
@@ -0,0 +1,13 @@
+(ns Problem4)
+
+(defn palindrome? [s]
+  (let [s (str s) r (apply str (reverse s))]
+    (= s r)))
+
+(defn largest []
+  (apply max 
+         (filter palindrome? 
+                 (for [a (range 100 1000) b (range 100 1000)]
+                      (* a b)))))
+
+(println (largest))
