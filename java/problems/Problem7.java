fd3db12c9742384e372cd7a7a3ed9151e7270b90
David Woods <dwoodsnz@gmail.com>
2011-07-11 21:00:51 +1000

    changed tabs to spaces in java p7 and Util

--- java/problems/Problem7.java
+++ java/problems/Problem7.java
@@ -4,18 +4,18 @@ import projecteuler.utility.Util;
 
 public class Problem7 {
 
-	 public static void main(String[] args) {
-		 int numPrimes = 10001;
-		 int primeCount = 0;
-		 
-		 int n = 1;
-		 while (primeCount < numPrimes) {
-			 n++;
-			 if (Util.isPrime(n)) {
-				 primeCount++;
-			 }
-		 }
-		
-		 System.out.println("target prime = " + n);
-	 }
+    public static void main(String[] args) {
+         int numPrimes = 10001;
+         int primeCount = 0;
+         
+         int n = 1;
+         while (primeCount < numPrimes) {
+             n++;
+             if (Util.isPrime(n)) {
+                 primeCount++;
+             }
+         }
+        
+         System.out.println("target prime = " + n);
+     }
 }
--- java/utility/Util.java
+++ java/utility/Util.java
@@ -1,13 +1,13 @@
 package projecteuler.utility;
 
 public class Util {
-	
-	 public static boolean isPrime(long n) {
+    
+    public static boolean isPrime(long n) {
          for (long i=2; i<=Math.sqrt(n); i++) {
              if (n % i == 0) {
                  return false;
              }
          }
          return true;
-	 }
+    }
 }
