fd13fe7b6d831e5481cdb2fe6e99abd4936e6c54
David Woods <dwoodsnz@gmail.com>
2011-07-14 20:01:48 +1000

    renamed all the clojure files to put problem numbers in form 001 so that they sort properly

---
+++ clojure/Problem005.clj
@@ -0,0 +1,10 @@
+(ns Problem005)
+
+(defn smallest-divisible [x]
+  (let [divisors (vec (range 2 (inc x)))]
+    (loop [i x]
+      (if (every? #(= 0 (mod i %)) divisors)
+          i
+          (recur (+ i x))))))
+
+(println (smallest-divisible 20))
---
+++ clojure/Problem009.clj
@@ -0,0 +1,11 @@
+(ns Problem009)
+
+(defn triplet-sum-equals [x]
+  (map int
+    (first
+      (filter #(= x (apply + %))
+              (for [a (range 1 x) b (range a x)]
+                   (let [c (Math/sqrt (+ (* a a) (* b b)))]
+                        (list a b c)))))))
+ 
+(println (apply * (triplet-sum-equals 1000)))
